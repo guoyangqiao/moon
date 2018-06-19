289ced19a7fa8df251f823f01e05f70b82fd6e6e
guoyangqiao <944734785@qq.com>
2018-06-19 17:28:28 +0800

    init 2

---
+++ src/main/java/com/guoyangqiao/moon/controller/MainCtl.java
@@ -0,0 +1,13 @@
+package com.guoyangqiao.moon.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/handle")
+public class MainCtl {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainCtl.class);
+
+}
